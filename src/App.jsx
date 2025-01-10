de822ba0f10646df7750e7f2272f1a5ed4e9400c
brand0ny0rk <brand0ny0rk@icloud.com>
2025-01-09 20:13:23 -0700

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css'
+import './App.css';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <>
       <h1>brand0ny0rk</h1>
+      <Hello />
     </>
   )
 }
---
+++ src/components/Hello.jsx
@@ -0,0 +1,5 @@
+function Hello() {
+  return <p>Hello, World!</p>;
+} 
+
+export default Hello;
